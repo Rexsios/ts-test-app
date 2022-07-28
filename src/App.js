d1fe1f29f1cf98919a0a661dd1cb5f932a716353
Slawomir Palka <slawomir.palka@miquido.com>
2022-07-28 10:28:10 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('olaboga conflikt!')
   return (
     <div className="App">
       <header className="App-header">
